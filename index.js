865ebfe387d1c4ad7c1f4202ab82ac03af90cc2b
ashim8 <ashimshil54321@gmail.com>
2022-05-28 23:54:31 +0600

    data work

--- index.js
+++ index.js
@@ -77,14 +77,16 @@ async function run() {
       const products = await cursor.toArray();
       res.send(products)
   });
- 
+  app.get('/product/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const product = await productCollection.findOne(query);
+      res.send(product);
+  })
 
     //end
 
-    app.get('/user', verifyJWT, async (req, res) => {
-      const users = await userCollection.find().toArray();
-      res.send(users);
-    });
+   
 
     app.get('/admin/:email', async (req, res) => {
       const email = req.params.email;
