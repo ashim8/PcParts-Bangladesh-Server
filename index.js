03946c7970575cfb5f0b40ae61ef2b3bcbd17fda
ashim8 <ashimshil54321@gmail.com>
2022-05-28 23:53:34 +0600

    some work

--- index.js
+++ index.js
@@ -71,12 +71,7 @@ async function run() {
       res.send(services);
     });
     //new
-    app.get('/product', async(req, res)=>{
-      const query = {};
-      const cursor = productCollection.find(query);
-      const products = await cursor.toArray();
-      res.send(products)
-  });
+   
   app.get('/product/:id', async(req,res)=>{
       const id = req.params.id;
       const query = {_id: ObjectId(id)};
