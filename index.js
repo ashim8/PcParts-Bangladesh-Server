0b67c4dc0b31fedf6267507828ade2668a759a7c
ashim8 <ashimshil54321@gmail.com>
2022-05-28 23:54:56 +0600

    data getting

--- index.js
+++ index.js
@@ -86,14 +86,11 @@ async function run() {
 
     //end
 
-   
+    app.get('/user', verifyJWT, async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
 
-    app.get('/admin/:email', async (req, res) => {
-      const email = req.params.email;
-      const user = await userCollection.findOne({ email: email });
-      const isAdmin = user.role === 'admin';
-      res.send({ admin: isAdmin })
-    })
 
     app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
       const email = req.params.email;
